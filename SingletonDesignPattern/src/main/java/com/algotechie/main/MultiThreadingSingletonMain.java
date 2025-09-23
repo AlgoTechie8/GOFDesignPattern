bae8d496d877a0818164131e9ed165acb48c7336
AlgoTeche <tech.vitthal@gmail.com>
2025-09-23 16:57:28 +0530

    solved using synchronized method

--- SingletonDesignPattern/src/main/java/com/algotechie/main/MultiThreadingSingletonMain.java
+++ SingletonDesignPattern/src/main/java/com/algotechie/main/MultiThreadingSingletonMain.java
@@ -1,9 +1,23 @@
 package com.algotechie.main;
 
+import com.algotechie.singleton.MultiThreadingSingleton;
+
 //09. Multi-Threading
 public class MultiThreadingSingletonMain {
 
 	public static void main(String[] args) {
 		
+		MultiThreadingSingleton obj1 =  MultiThreadingSingleton.getInstance();
+		System.out.println("Object 1 hashcode : "+obj1.hashCode());
+		System.out.println();
+		
+		Runnable task = () -> {
+			MultiThreadingSingleton obj2 = MultiThreadingSingleton.getInstance();
+			System.out.println("Thread got instance: " + obj2.hashCode());
+		};
+		Thread t1 = new Thread(task);
+		Thread t2 = new Thread(task);
+		// may print different hashCodes so resolve this issue using synchronized method or synchronized block double checking
+		t1.start(); t2.start(); 
 	}
 }
--- SingletonDesignPattern/src/main/java/com/algotechie/singleton/MultiThreadingSingleton.java
+++ SingletonDesignPattern/src/main/java/com/algotechie/singleton/MultiThreadingSingleton.java
@@ -2,6 +2,21 @@ package com.algotechie.singleton;
 
 //09. Multi-Threading
 public class MultiThreadingSingleton {
+	// Declare same class-type member as private, static
+	private static MultiThreadingSingleton instance;
 	
+	// Declare Constructor as private
+	private MultiThreadingSingleton() {
+		// No Operation
+	}
+	
+	// Declare synchronized, static factory method to create instance of same class only once
+	// Synchronized method (simplest but slower)
+	public synchronized static MultiThreadingSingleton getInstance() {
+		if (instance == null) {
+			instance = new MultiThreadingSingleton();
+		}
+		return instance;
+	}
 
 }
