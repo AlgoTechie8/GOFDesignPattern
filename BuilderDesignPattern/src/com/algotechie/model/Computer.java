9cd47e80608ba21788c8fcb1affb31bf296c3697
AlgoTeche <tech.vitthal@gmail.com>
2025-09-24 18:37:00 +0530

    created builder dp application

---
+++ BuilderDesignPattern/src/com/algotechie/model/Computer.java
@@ -0,0 +1,17 @@
+package com.algotechie.model;
+
+public class Computer {
+	// Required
+	private String cpu;
+	private String ram;
+	
+	// Optional
+	private String gpu;
+	private String storege;
+	private boolean wifiEnabled;
+	private boolean bluetoothEnabled;
+	
+	 // Private constructor to enforce Builder usage
+	
+
+}
